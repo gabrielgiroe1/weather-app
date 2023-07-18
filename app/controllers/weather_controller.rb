032a1d6a6ef02aa08a25171d9aaf553bb01f4631
gabrielgiroe1 <gabriel.giroe@gmail.com>
2023-07-18 18:36:59 +0300

    Refactoring

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,8 +4,6 @@ class WeatherController < ApplicationController
     if params[:location].present?
       response = HTTParty.get("https://api.weatherapi.com/v1/current.json?key=#{ENV['WEATHER_KEY']}&q=#{params[:location]}")
       weather_data = JSON.parse(response.body)
-      debugger
-
       if response.code == 200 && weather_data.present?
         @weather_data = {
           temperature: weather_data['current']['temp_c'],
